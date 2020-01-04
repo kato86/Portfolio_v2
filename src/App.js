0b6b5a8cf70c7cfe16ec7d8b435d3ec35d9dab9d
MATRIX\Kamil <tomala.kamil@gmail.com>
2020-01-04 17:07:27 +0100

    Add MySite container to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./scss/main.scss";
 
+import MySite from "./containers/MySite/MySite";
+
 function App() {
   return (
     <div className="App">
       <h1>Site in progres...</h1>
+      <MySite />
     </div>
   );
 }
