31b28a6d6ddc2bf92a1ffc5a85e90176bfd3bd04
MATRIX\Kamil <tomala.kamil@gmail.com>
2020-01-04 17:18:34 +0100

    Add scss to AboutMe

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,5 +1,7 @@
 import React from "react";
 
-const aboutMe = props => <h1>AboutMe section</h1>;
+import "../../scss/AboutMe.scss";
+
+const aboutMe = props => <h1 className="AboutMe">AboutMe section</h1>;
 
 export default aboutMe;
