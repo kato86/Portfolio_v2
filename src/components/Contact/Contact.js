fc668a49473e02fbab29efad63774cfaaa5944ed
MATRIX\Kamil <tomala.kamil@gmail.com>
2020-01-07 23:09:30 +0100

    Add Contact component

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "../../scss/Contact.scss";
+
+const contact = () => <h1 className="Contact">Contact component</h1>;
+
+export default contact;
--- src/containers/MySite/MySite.js
+++ src/containers/MySite/MySite.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 import AboutMe from "../../components/AboutMe/AboutMe";
+import Contact from "../../components/Contact/Contact";
 
 const mySite = props => (
   <div>
     <AboutMe />
+    <Contact />
   </div>
 );
 
