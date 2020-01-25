4cc639cb68fb5390cdc0feabe7c3d11b4ccbf084
MATRIX\Kamil <tomala.kamil@gmail.com>
2020-01-25 22:46:37 +0100

    Import Navigation to MySite component

--- src/containers/MySite/MySite.js
+++ src/containers/MySite/MySite.js
@@ -2,9 +2,11 @@ import React from "react";
 
 import AboutMe from "../../components/AboutMe/AboutMe";
 import Contact from "../../components/Contact/Contact";
+import Navigation from "../../components/Navigation/Navigation";
 
 const mySite = props => (
   <div>
+    <Navigation />
     <AboutMe />
     <Contact />
   </div>
