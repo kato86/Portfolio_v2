4b8c32323f49044dbfe62a591fa7072650e860a4
MATRIX\Kamil <tomala.kamil@gmail.com>
2020-01-26 23:28:15 +0100

    Import footer to MySite

--- src/containers/MySite/MySite.js
+++ src/containers/MySite/MySite.js
@@ -3,12 +3,14 @@ import React from "react";
 import AboutMe from "../../components/AboutMe/AboutMe";
 import Contact from "../../components/Contact/Contact";
 import Navigation from "../../components/Navigation/Navigation";
+import Footer from "../../components/Footer/Footer";
 
 const mySite = props => (
   <div>
     <Navigation />
     <AboutMe />
     <Contact />
+    <Footer />
   </div>
 );
 
