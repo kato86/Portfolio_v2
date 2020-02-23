e96a6e676db523dc24568c0687c4ea50ca88b4a4
MATRIX\Kamil <tomala.kamil@gmail.com>
2020-02-23 22:03:08 +0100

    Add name to AboutMe section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -2,6 +2,11 @@ import React from "react";
 
 import "../../scss/components/AboutMe.scss";
 
-const aboutMe = props => <div className="AboutMe">AboutMe section</div>;
+const aboutMe = props => (
+  <div className="AboutMe">
+    <h1>Kamil Tomala</h1>
+    <p>WebDeveloper</p>
+  </div>
+);
 
 export default aboutMe;
