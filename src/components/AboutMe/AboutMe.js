6be20c33b5dcd3579e6643f577d1486b5abefd6b
MATRIX\Kamil <tomala.kamil@gmail.com>
2020-02-25 22:37:56 +0100

    Add css to my name

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -4,7 +4,7 @@ import "../../scss/components/AboutMe.scss";
 
 const aboutMe = props => (
   <div className="AboutMe">
-    <h1>Kamil Tomala</h1>
+    <h1 className="Name">Kamil Tomala</h1>
     <p>WebDeveloper</p>
   </div>
 );
