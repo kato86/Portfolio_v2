2b8becc5ea005f23a74a635d7f04088c1dc6530a
MATRIX\Kamil <tomala.kamil@gmail.com>
2020-04-15 22:48:22 +0200

    Delete footer

--- src/containers/MySite/MySite.js
+++ src/containers/MySite/MySite.js
@@ -3,14 +3,12 @@ import React from "react";
 import AboutMe from "../../components/AboutMe/AboutMe";
 import Contact from "../../components/Contact/Contact";
 import Navigation from "../../components/Navigation/Navigation";
-import Footer from "../../components/Footer/Footer";
 
-const mySite = props => (
+const mySite = (props) => (
   <div>
     <Navigation />
     <AboutMe />
     <Contact />
-    <Footer />
   </div>
 );
 
