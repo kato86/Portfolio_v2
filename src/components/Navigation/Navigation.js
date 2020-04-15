1e6a4e8521d1a4fc1b91b747b2e1c969c5a160ea
MATRIX\Kamil <tomala.kamil@gmail.com>
2020-04-15 22:48:52 +0200

    Delete navigation

--- src/components/Navigation/Navigation.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const navigation = (props) => (
-  <div className="Navigation">
-    <nav>Navigation !!!</nav>
-  </div>
-);
-
-export default navigation;
--- src/containers/MySite/MySite.js
+++ src/containers/MySite/MySite.js
@@ -2,11 +2,9 @@ import React from "react";
 
 import AboutMe from "../../components/AboutMe/AboutMe";
 import Contact from "../../components/Contact/Contact";
-import Navigation from "../../components/Navigation/Navigation";
 
 const mySite = (props) => (
   <div>
-    <Navigation />
     <AboutMe />
     <Contact />
   </div>
