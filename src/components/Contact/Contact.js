1448ccc594ce225cd6a4582fdcfd90221a52a4e2
MATRIX\Kamil <tomala.kamil@gmail.com>
2020-04-15 22:49:20 +0200

    Delete contact

--- src/components/Contact/Contact.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const contact = () => <div className="Contact">Contact component</div>;
-
-export default contact;
--- src/containers/MySite/MySite.js
+++ src/containers/MySite/MySite.js
@@ -1,12 +1,10 @@
 import React from "react";
 
 import AboutMe from "../../components/AboutMe/AboutMe";
-import Contact from "../../components/Contact/Contact";
 
 const mySite = (props) => (
   <div>
     <AboutMe />
-    <Contact />
   </div>
 );
 
